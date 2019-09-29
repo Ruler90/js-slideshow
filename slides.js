ff9852bc2b03f24286e0b738f00c54aebe082821
Ruler90 <54283970+Ruler90@users.noreply.github.com>
2019-09-29 22:29:11 +0200

    Update slides.js

--- slides.js
+++ slides.js
@@ -4,14 +4,14 @@ const time = +getComputedStyle(document.querySelector('.animation--time')).anima
 function nextSlide () {
   for (let i = 0; i < slides.length; i++) {
     if (slides[slides.length - 1].classList.contains('activeSlide')) {
-      slides[0].setAttribute('class', 'mainSlides animation animation--time animation__slideFromRightToLeft activeSlide');
-      slides[slides.length - 1].setAttribute('class', 'mainSlides animation animation--time animation__slideFromZeroToLeft');
+      slides[0].classList.add('animation', 'animation--time', 'animation__slideFromRightToLeft', 'activeSlide');
+      slides[slides.length - 1].classList.add('animation', 'animation--time', 'animation__slideFromZeroToLeft');
       setTimeout(function () { slides[slides.length - 1].setAttribute('class', 'mainSlides') }, time);
       break;
     }
     if (slides[i].classList.contains('activeSlide')) {
-      slides[i + 1].setAttribute('class', 'mainSlides animation animation--time animation__slideFromRightToLeft activeSlide');
-      slides[i].setAttribute('class', 'mainSlides animation animation--time animation__slideFromZeroToLeft');
+      slides[i + 1].classList.add('animation', 'animation--time', 'animation__slideFromRightToLeft', 'activeSlide');
+      slides[i].classList.add('animation', 'animation--time', 'animation__slideFromZeroToLeft');
       setTimeout(function () { slides[i].setAttribute('class', 'mainSlides') }, time);
       break;
     }
