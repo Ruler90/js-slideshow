eb99f9acbdfe29e641b072c57d06333069f4693a
Ruler90 <54283970+Ruler90@users.noreply.github.com>
2019-09-29 21:54:02 +0200

    Update slides.js

--- slides.js
+++ slides.js
@@ -3,22 +3,16 @@ const time = +getComputedStyle(document.querySelector('.animation--time')).anima
 
 function nextSlide () {
   for (let i = 0; i < slides.length; i++) {
-    if (!slides[slides.length - 1].classList.contains('slidePic--hidden')) {
-      setTimeout(function () { slides[slides.length - 1].classList.add('slidePic--hidden') }, time);
-      slides[slides.length - 1].classList.add('animation', 'animation--time', 'animation__slideFromZeroToLeft');
-      setTimeout(function () { slides[slides.length - 1].classList.remove('animation', 'animation--time', 'animation__slideFromZeroToLeft') }, time);
-      slides[0].classList.remove('slidePic--hidden');
-      slides[0].classList.add('animation', 'animation--time', 'animation__slideFromRightToLeft');
-      setTimeout(function () { slides[0].classList.remove('animation', 'animation--time', 'animation__slideFromRightToLeft') }, time);
+    if (slides[slides.length - 1].classList.contains('activeSlide')) {
+      slides[0].setAttribute('class', 'mainSlides animation animation--time animation__slideFromRightToLeft activeSlide');
+      slides[slides.length - 1].setAttribute('class', 'mainSlides animation animation--time animation__slideFromZeroToLeft');
+      setTimeout(function () { slides[slides.length - 1].setAttribute('class', 'mainSlides') }, time);
       break;
     }
-    if (!slides[i].classList.contains('slidePic--hidden')) {
-      slides[i].classList.add('animation', 'animation--time', 'animation__slideFromZeroToLeft');
-      setTimeout(function () { slides[i].classList.remove('animation', 'animation--time', 'animation__slideFromZeroToLeft') }, time);
-      setTimeout(function () { slides[i].classList.add('slidePic--hidden') }, time);
-      slides[i + 1].classList.remove('slidePic--hidden');
-      slides[i + 1].classList.add('animation', 'animation--time', 'animation__slideFromRightToLeft');
-      setTimeout(function () { slides[i + 1].classList.remove('animation', 'animation--time', 'animation__slideFromRightToLeft') }, time);
+    if (slides[i].classList.contains('activeSlide')) {
+      slides[i + 1].setAttribute('class', 'mainSlides animation animation--time animation__slideFromRightToLeft activeSlide');
+      slides[i].setAttribute('class', 'mainSlides animation animation--time animation__slideFromZeroToLeft');
+      setTimeout(function () { slides[i].setAttribute('class', 'mainSlides') }, time);
       break;
     }
   }
@@ -30,22 +24,16 @@ let autoChange = setInterval(nextSlide, 4000);
 
 function previousSlide () {
   for (let i = 0; i < slides.length; i++) {
-    if (!slides[0].classList.contains('slidePic--hidden')) {
-      setTimeout(function () { slides[0].classList.add('slidePic--hidden') }, time);
-      slides[0].classList.add('animation', 'animation--time', 'animation__slideFromZeroToRight');
-      setTimeout(function () { slides[0].classList.remove('animation', 'animation--time', 'animation__slideFromZeroToRight') }, time);
-      slides[slides.length - 1].classList.remove('slidePic--hidden');
-      slides[slides.length - 1].classList.add('animation', 'animation--time', 'animation__slideFromLeftToRight');
-      setTimeout(function () { slides[slides.length - 1].classList.remove('animation', 'animation--time', 'animation__slideFromLeftToRight') }, time);
+    if (slides[0].classList.contains('activeSlide')) {
+      slides[slides.length - 1].setAttribute('class', 'mainSlides animation animation--time animation__slideFromLeftToRight activeSlide');
+      slides[0].setAttribute('class', 'mainSlides animation animation--time animation__slideFromZeroToRight');
+      setTimeout(function () { slides[0].setAttribute('class', 'mainSlides') }, time);
       break;
     }
-    if (!slides[i].classList.contains('slidePic--hidden')) {
-      setTimeout(function () { slides[i].classList.add('slidePic--hidden') }, time);
-      slides[i].classList.add('animation', 'animation--time', 'animation__slideFromZeroToRight');
-      setTimeout(function () { slides[i].classList.remove('animation', 'animation--time', 'animation__slideFromZeroToRight') }, time)
-      slides[i - 1].classList.remove('slidePic--hidden');
-      slides[i - 1].classList.add('animation', 'animation--time', 'animation__slideFromLeftToRight');
-      setTimeout(function () { slides[i - 1].classList.remove('animation', 'animation--time', 'animation__slideFromLeftToRight') }, time);
+    if (slides[i].classList.contains('activeSlide')) {
+      slides[i - 1].setAttribute('class', 'mainSlides animation animation--time animation__slideFromLeftToRight activeSlide');
+      slides[i].setAttribute('class', 'mainSlides animation animation--time animation__slideFromZeroToRight');
+      setTimeout(function () { slides[i].setAttribute('class', 'mainSlides') }, time);
       break;
     }
   }
